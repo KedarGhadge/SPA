aaa3863c5b228f77caf72e69e0d1e6a4df717fda
KedarGhadge <kghadge84@gmail.com>
2022-07-28 00:04:42 +0530

    Made changes for canvas

--- README.md
+++ README.md
@@ -8,6 +8,29 @@
 
 [![Generic badge](https://img.shields.io/badge/Project-SPA-yellow.svg?style=for-the-badge)](https://shields.io/)
 
+<style>
+details{
+      cursor: pointer;
+  position: relative;
+  padding: 12px 8px 12px 40px;
+  list-style-type: none;
+  background: linear-gradient(145deg, #fdfbfb, #eceeef);
+  font-size: 18px;
+  transition: 0.2s;
+  border-radius: 4px;
+  box-shadow: -3px -3px 7px #bebebe, 3px 3px 7px #ffffff;
+  border-bottom: 1px solid #22222242;
+
+  /* make the list items unselectable */
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+
+</style>
+
 # Introduction
 
 - Created project for the study purpose of the Single page applications using knockout js
@@ -21,9 +44,17 @@
 | 2     | Make an page for the image editing | ![](https://progress-bar.dev/30)  |
 | 3     | Add section to add image           | ![](https://progress-bar.dev/100) |
 | 4     | add section to editing pallette    | ![](https://progress-bar.dev/0)   |
+| 5     | Add canvas for better editing      | ![](https://progress-bar.dev/100) |
 
-# Changes made
+# Changes Log
 
+<details >
+<summary style="color:#5a6999">[07/27/2022 08:39pm]</summary>
+
+- Added canvas and removed old div tag.
+- Image is directly showing on the canvas after selecting.
+
+</details>
 <details >
 <summary style="color:#5a6999">[07/03/2022 04:55pm]</summary>
 
--- src/components/Image-holder/image_holder.js
+++ src/components/Image-holder/image_holder.js
@@ -7,17 +7,38 @@ class ImageHolder {
     // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
     // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
     this.route = params.route;
-    const image_input = document.querySelector("#image-input");
-    image_input.addEventListener("change", function () {
-      const reader = new FileReader();
-      reader.addEventListener("load", () => {
-        const uploaded_image = reader.result;
-        document.querySelector(
-          "#display-image"
-        ).style.backgroundImage = `url(${uploaded_image})`;
-      });
-      reader.readAsDataURL(this.files[0]);
-    });
+    // const image_input = document.querySelector("#image-input");
+    // image_input.addEventListener("change", function () {
+    //   const reader = new FileReader();
+    //   reader.addEventListener("load", () => {
+    //     const uploaded_image = reader.result;
+    //     document.querySelector(
+    //       "#display-image"
+    //     ).style.backgroundImage = `url(${uploaded_image})`;
+    //   });
+    //   reader.readAsDataURL(this.files[0]);
+    // });
+    // var c = document.getElementById("myCanvas");
+    // var ctx = c.getContext("2d");
+    // ctx.font = "30px Arial";
+    // ctx.drawImage(reader, 10, 50);
+  }
+  handleImage(e) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    // ctx.font = "30px Arial";
+    // ctx.drawImage(reader, 10, 50);
+    var reader = new FileReader();
+    reader.onload = function (event) {
+      var img = new Image();
+      img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+      };
+      img.src = event.target.result;
+    };
+    reader.readAsDataURL(e);
   }
 }
 
