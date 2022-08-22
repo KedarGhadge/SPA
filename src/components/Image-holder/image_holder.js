ec4ba9b29378b94ed3cca7236b2110ecd601ceec
KedarGhadge <kghadge84@gmail.com>
2022-08-22 20:29:25 +0530

    invert image changes

--- src/components/Image-holder/image_holder.js
+++ src/components/Image-holder/image_holder.js
@@ -61,6 +61,33 @@ class ImageHolder {
       }
       ctx.putImageData(imageData, 0, 0);
     };
+    // self.changeOpacity = function () {
+    //   var imageData = ctx.getImageData(
+    //     0,
+    //     0,
+    //     self.imageWidth(),
+    //     self.imageHeight()
+    //   );
+    //   ctx.save();
+    //   ctx.globalAlpha = 0.4;
+    //   ctx.putImageData(imageData, 0, 0);
+    //   ctx.restore();
+    // };
+    self.invertImage = function () {
+      var imageData = ctx.getImageData(
+        0,
+        0,
+        self.imageWidth(),
+        self.imageHeight()
+      );
+      var data = imageData.data;
+      for (var i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i]; // red
+        data[i + 1] = 255 - data[i + 1]; // green
+        data[i + 2] = 255 - data[i + 2]; // blue
+      }
+      ctx.putImageData(imageData, 0, 0);
+    };
     self.downloadImage = function () {
       var anchor = document.createElement("a");
       anchor.href = canvas.toDataURL("image/png");
